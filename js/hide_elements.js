a7efc45ab72126bc4858d799311c4f47049757b6
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2020-08-19 16:56:19 +1000

    Hides CTA title

--- js/hide_elements.js
+++ js/hide_elements.js
@@ -5,6 +5,7 @@
   Drupal.behaviors.hideParagraphsElements = {
     attach: function (context, settings) {
       $(context).find("table[id^=field-paragraph-keydates-values--] h4").hide()
+      $(context).find("div[id^=edit-field-landing-page-component-] fieldset legend").hide()
     }
   };
 })(jQuery, Drupal);
