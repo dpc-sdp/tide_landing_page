46701d9cdd34f0a4644365f0278e64f21a60db8c
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2020-08-25 14:52:47 +1000

    Update js to hide CTA

--- js/hide_elements.js
+++ js/hide_elements.js
@@ -5,6 +5,11 @@
   Drupal.behaviors.hideParagraphsElements = {
     attach: function (context, settings) {
       $(context).find("table[id^=field-paragraph-keydates-values--] h4").hide()
+      $(context).find("div[id^=edit-field-landing-page-component-] fieldset legend span").each(function () {
+        if ($(this).text() === "CTA") {
+          $(this).hide();
+        }
+      })
     }
   };
 })(jQuery, Drupal);
