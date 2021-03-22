9a822876b9b14d33d7e68fbe98fd4b5e3f90abf0
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2021-03-23 08:47:52 +1100

    adds a update hook to delete old cards

--- tide_landing_page.post_update.php
+++ tide_landing_page.post_update.php
@@ -12,26 +12,6 @@ use Drupal\paragraphs\Entity\Paragraph;
  * Removes old paragraph types.
  */
 function tide_landing_page_post_update_remove_old_paragraph_types() {
-  // Makes sure that all cards are deleted.
-  $paragraph_storage = \Drupal::entityTypeManager()->getStorage('paragraph');
-  $query = \Drupal::entityQuery('paragraph')
-    ->condition('parent_type', 'node')
-    ->condition('parent_field_name', 'field_landing_page_component')
-    ->condition('parent_type', 'node');
-  $or = $query->orConditionGroup();
-  $or->condition('type', 'card_navigation')
-    ->condition('type', 'card_navigation_auto')
-    ->condition('type', 'card_navigation_featured')
-    ->condition('type', 'card_navigation_featured_auto')
-    ->condition('type', 'card_promotion')
-    ->condition('type', 'card_promotion_auto')
-    ->condition('type', 'card_event')
-    ->condition('type', 'card_event_auto');
-  $results = $query->condition($or)->execute();
-  if (!empty($results)) {
-    $paragraph_storage->delete(Paragraph::loadMultiple($results));
-  }
-
   // Deletes cards definitions.
   $paragraph_types = [
     'card_navigation_featured_auto',
