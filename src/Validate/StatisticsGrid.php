e91bf7d0fde1467ccd82bd50421fac8e9ab48fb2
MdNadimHossain <getovic@gmail.com>
2022-02-14 18:54:18 +1100

    Added statistics grid component.

---
+++ src/Validate/StatisticsGrid.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\tide_landing_page\Validate;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Validation handler for statistics grid component.
+ */
+class StatisticsGrid {
+  /**
+   * Validates given element.
+   *
+   * @param array $element
+   *   The paragraph element to process.
+   * @param \Drupal\Core\Form\FormStateInterface $formState
+   *   The form state.
+   * @param array $form
+   *   The complete form structure.
+   */
+  public static function validate(array &$element, FormStateInterface $formState, array &$form)
+  {
+    $error = FALSE;
+    $max_delta = $element['subform']['field_statistic_block']['widget']['#max_delta'];
+    if ($max_delta == 0) {
+      $error = TRUE;
+    }
+    if ($error) {
+      $formState->setError(
+        $element,
+        new TranslatableMarkup('The statistics grid component needs minimum two blocks to be added.')
+      );
+    }
+  }
+}
