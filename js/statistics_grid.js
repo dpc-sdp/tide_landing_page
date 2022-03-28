172d2b11bbc075036dd6e38bcca183231ffee821
Mayur Gondhkar <mayurngondhkar@gmail.com>
2022-03-28 22:22:54 +1100

    [SDPA-6050] Display 2 statistic grid blocks by default

--- js/statistics_grid.js
+++ js/statistics_grid.js
@@ -8,6 +8,14 @@
       if(statisticsGridBlockCount) {
         $('.paragraph-type--statistics-grid').find('span[class*=paragraphs-badge]').html(statisticsGridBlockCount);
       }
+
+      // Display a default of 2 statistics-grid blocks
+      if(statisticsGridBlockCount === 1) {
+        // Hide the first statistics-grid block while the second one loads
+        $('.paragraph-type--statistics-grid').find('tr[class*=draggable]').hide()
+        // Add another statistic grid block
+        $(context).find("input[id*=field-statistic-block-add-more-add-more-button-statistic-block]").click();
+      }
     }
   };
 })(jQuery, Drupal);
