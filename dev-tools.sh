a23a5e4268e53b5e95681134c6015156619cef15
Md Nadim Hossain <getovic@gmail.com>
2023-11-15 17:13:34 +1100

    Reverted test changes.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-export GH_COMMIT=7a39673f5d374d71fc3cd69114b2b9914ed86a6c
+# export GH_COMMIT=COMMIT_SHA
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
